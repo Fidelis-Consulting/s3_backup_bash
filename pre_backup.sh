851658a325d829ed60ceaf06bb8a3fd4c994f26d
Mark Saum <mark@fidelisconsulting.com>
2017-09-11 12:03:09 -0500

    Create pre_backup.sh

---
+++ pre_backup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# -------------------------------------------------------------------------------
+# Name: pre_backup.sh
+# Purpose: Backup helper file
+# Version: 1.0
+# Date: May 25, 2017
+# Author: Mark Saum
+# -------------------------------------------------------------------------------
+
+echo "Pre Backup: Nothing done."
+
