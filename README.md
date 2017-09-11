1dad1609dd80266c2497a8ad1e30527e987096ca
Mark Saum <mark@fidelisconsulting.com>
2017-09-11 16:08:03 -0500

    Fixed minor bugs. Added enhanced error checking. Added a config file option.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # s3_backup_bash
 S3 Backup Script written in bash
+
+# Requirements:
+ - AWS client installed and configured
+ - AWS credentials or role assigned to instance
+ - S3 Bucket and permissions to role
+
+# Installation
+ Script installs in: ```/usr/local/bin/backup.sh```
+ 
+ Transform installs in: ```/usr/local/etc/backup_transform```
+ 
+ Pre and Post backup scripts are optional and are installed in:
+ 
+```
+/usr/local/bin/pre_backup.sh  
+/usr/local/bin/post_backup.sh
+```
+
+## Scheduling via Cron
+To schedule a backup via cron, an entry like this one should be used:
+
+``````
+
+```
+# minute   hour   dayOfMonth   Month   dayOfWeek   commandToRun
+15         0      *            *       *           /usr/local/bin/backup.sh
+```
+
+## Transforms
+The transform file defines what files and/or directories are backed up.
+
+## Pre and Post Backup Scripts
+Pre and Post backup scripts are custom scripts that are run before or after a backup.  
+End users are responsible for writing these scripts to meet any custom needs such as stopping and restarting a database server, etc.
+
+## AWS Permissions
+The host or AWS instance will need access to the AWS bucket specificed.
+
+This is most easily done in AWS with a role that provides access to the bucket.  However, configuring the AWS client with a set of keys will also work. 
+
--- backup.sh
+++ backup.sh
@@ -5,6 +5,7 @@
 # Version: 1.0
 # Date: May 25, 2017
 # Author: Mark Saum
+# GitHub: https://github.com/msaum
 # -------------------------------------------------------------------------------
 # Requirements:
 # - AWS client installed and configured
@@ -14,38 +15,53 @@
 # Installation:
 # Script installs in: /usr/local/bin/backup.sh
 # Transform installs in: /usr/local/etc/backup_transform
+# Configuration installs in: /usr/local/etc/backup.conf
 # Pre and Post backup scripts are optional and are installed in:
 # /usr/local/bin/pre_backup.sh
 # /usr/local/bin/post_backup.sh
 # -------------------------------------------------------------------------------
 
+echo "** Starting: backup.sh"
+######################
+# exit_error()
+function exit_error() {
+    if [ -f ${BACKUP_TEMP_FILE} ]; then
+        rm -f ${BACKUP_TEMP_FILE}
+    fi
+    echo "** Falure: $1"
+    echo "** Ending: backup.sh"
+    exit 1
+}
+
 ######################
 # Program variables
-HOSTNAME=`which hostname`
-GAWK=`which gawk`
-CURL=`which curl`
-DU=`which du`
-TAR=`which tar`
-WC=`which wc`
-AWSCMD=`which aws`
+HOSTNAME=`which hostname`  || exit_error "Can't find hostname"
+GAWK=`which gawk`  || exit_error "Can't find gawk"
+CURL=`which curl`  || exit_error "Can't find curl"
+DU=`which du`  || exit_error "Can't find du"
+TAR=`which tar`  || exit_error "Can't find tar"
+WC=`which wc`  || exit_error "Can't find wc"
+AWSCMD=`which aws`  || exit_error "Can't find awscli"
 S3CMD="${AWSCMD} s3 cp"
-#BACKUP_COMPRESSION="z"
-BACKUP_COMPRESSION="j"
-#VERBOSE="v"
-
-# Chat Details
-BACKUP_MSG_ROOM_ID="Backups"
-BACKUP_MSG_FROM="Backup Bot"
-BACKUP_MSG_AUTHTOKEN=""
-
-# Backup Details
-BACKUP_PRE_SCRIPT=/usr/local/bin/pre_backup.sh
-BACKUP_POST_SCRIPT=/usr/local/bin/post_backup.sh
-BACKUP_REPO="s3://backups"
-BACKUP_TRANSFORM="/usr/local/etc/backup_transform"
 BACKUP_HOSTNAME=`${HOSTNAME}`
 BACKUP_DOMAIN=`${HOSTNAME} -d`
-TEMP_BACKUP_FILE=$(mktemp /var/tmp/backup-XXXXXXXXXX) || { echo "Failed to create temp file"; exit 1; }
+BACKUP_TEMP_FILE=$(mktemp /var/tmp/backup-XXXXXXXXXX) || exit_error "Failed to create temp file"
+
+######################
+# Read Config File
+shopt -s extglob
+CONFIGFILE="/usr/local/etc/backup.conf"
+while IFS='= ' read lhs rhs
+do
+    if [[ ! $lhs =~ ^\ *# && -n $lhs ]]; then
+        rhs="${rhs%%\#*}"    # Del in line right comments
+        rhs="${rhs%%*( )}"   # Del trailing spaces
+        rhs="${rhs%\"}"     # Del opening string quotes
+        rhs="${rhs#\"}"     # Del closing string quotes
+        declare $lhs="$rhs"
+    fi
+done < ${CONFIGFILE}
+
 BACKUP_FILE=`date +'%Y-%m-%d-%H'`.tb${BACKUP_COMPRESSION}
 
 ######################
@@ -57,16 +73,23 @@ trap ctrl_c INT
 function exit_function() {
     ###########################
     # Remove backup file
-    rm -f ${TEMP_BACKUP_FILE}
-    exit
+    rm -f ${BACKUP_TEMP_FILE}
+    echo "** Ending: backup.sh"
+    exit 0
 }
 
 ######################
 # ctrl_c()
 function ctrl_c() {
-        echo "** Trapped CTRL-C"
+        echo "** Interrupted: Trapped CTRL-C"
         exit_function
 }
+
+###########################
+# Check for Transform file
+if [ ! -f ${BACKUP_TRANSFORM} ]; then
+    exit_error "Backup Transform file: \"${BACKUP_TRANSFORM}\" not found!"
+fi
 
 ###########################
 # Run pre-backup script
@@ -77,19 +100,18 @@ fi
 ###########################
 # Create backup file
 cd /
-echo "** Running: ${TAR} c${VERBOSE}f${BACKUP_COMPRESSION} ${TEMP_BACKUP_FILE} -T ${BACKUP_TRANSFORM}"
-${TAR} c${VERBOSE}f${BACKUP_COMPRESSION} ${TEMP_BACKUP_FILE} -T ${BACKUP_TRANSFORM}
+echo "** Running: ${TAR} c${BACKUP_VERBOSE}f${BACKUP_COMPRESSION} ${BACKUP_TEMP_FILE} -T ${BACKUP_TRANSFORM}"
+${TAR} c${BACKUP_VERBOSE}f${BACKUP_COMPRESSION} ${BACKUP_TEMP_FILE} -T ${BACKUP_TRANSFORM} || exit_error "tar command failed"
 
 ###########################
 # Transfer backup to S3
-echo "** Running: ${S3CMD} ${TEMP_BACKUP_FILE} ${BACKUP_REPO}/${BACKUP_DOMAIN}/${BACKUP_HOSTNAME}/${BACKUP_FILE}"
-${S3CMD} ${TEMP_BACKUP_FILE} ${BACKUP_REPO}/${BACKUP_DOMAIN}/${BACKUP_HOSTNAME}/${BACKUP_FILE}
+echo "** Running: ${S3CMD} ${BACKUP_TEMP_FILE} ${BACKUP_REPO}/${BACKUP_DOMAIN}/${BACKUP_HOSTNAME}/${BACKUP_FILE}"
+${S3CMD} ${BACKUP_TEMP_FILE} ${BACKUP_REPO}/${BACKUP_DOMAIN}/${BACKUP_HOSTNAME}/${BACKUP_FILE} || exit_error "s3 file copy failed"
 
 ###########################
 # Get backup stats
-BACKUP_SIZE=`${DU} -sh ${TEMP_BACKUP_FILE} | ${GAWK} '{print $1}'`
-BACKUP_FILE=`${TAR} tvf ${TEMP_BACKUP_FILE} | ${WC} -l`
-
+BACKUP_SIZE=`${DU} -sh ${BACKUP_TEMP_FILE} | ${GAWK} '{print $1}'` || exit_error "Calculating backup file size failed"
+BACKUP_FILE_COUNT=`${TAR} tvf ${BACKUP_TEMP_FILE} | ${WC} -l` || exit_error "Calculating backup file count failed"
 
 ###########################
 # Run post-backup script
@@ -97,9 +119,11 @@ if [ -x ${BACKUP_POST_SCRIPT} ]; then
 	${BACKUP_POST_SCRIPT}
 fi
 
+echo "** Status: A backup of host ${BACKUP_HOSTNAME}.${BACKUP_DOMAIN} has completed which contains ${BACKUP_FILE_COUNT} files and is ${BACKUP_SIZE}."
+
 ###########################
 # Send status to HipChat
-#${CURL} --silent -d "room_id=${BACKUP_MSG_ROOM_ID}&from=${BACKUP_MSG_FROM}&message=A+backup+of+host+${BACKUP_HOSTNAME}.${BACKUP_DOMAIN}+has+completed+which+contains+${BACKUP_FILEs}+files+and+is+${BACKUP_SIZE}+in+size.&color=red&message_format=text&notify=0&auth_token=${BACKUP_MSG_AUTHTOKEN}"  https://api.hipchat.com/v1/rooms/message > /dev/null
+#${CURL} --silent -d "room_id=${BACKUP_MSG_ROOM_ID}&from=${BACKUP_MSG_FROM}&message=A+backup+of+host+${BACKUP_HOSTNAME}.${BACKUP_DOMAIN}+has+completed+which+contains+${BACKUP_FILE_COUNT}+files+and+is+${BACKUP_SIZE}+in+size.&color=red&message_format=text&notify=0&auth_token=${BACKUP_MSG_AUTHTOKEN}"  https://api.hipchat.com/v1/rooms/message > /dev/null
 
 exit_function
 
