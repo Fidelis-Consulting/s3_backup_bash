1981b4afc64cb6e52719cb1b9cb5bcb88b69b5f9
Mark Saum <mark@fidelisconsulting.com>
2017-09-11 16:12:51 -0500

    Fix broken line endings.

--- pre_backup.sh
+++ pre_backup.sh
@@ -1,1 +1,11 @@
-#!/usr/bin/env bash# -------------------------------------------------------------------------------# Name: pre_backup.sh# Purpose: Backup helper file# Version: 1.0# Date: May 25, 2017# Author: Mark Saum# -------------------------------------------------------------------------------echo "Pre Backup: Nothing done."
+#!/usr/bin/env bash
+# -------------------------------------------------------------------------------
+# Name: pre_backup.sh
+# Purpose: Backup helper file
+# Version: 1.0
+# Date: May 25, 2017
+# Author: Mark Saum
+# -------------------------------------------------------------------------------
+
+echo "Pre Backup: Nothing done."
+
